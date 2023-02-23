6beac55e7e4d898e6f10308d0e95754c6c2f030b
Olle Ronstad <olle.ronstad@gmail.com>
2023-02-23 14:23:53 +0100

    added code to situation as given in the instruction. note, public instead of private or package

--- src/main/java/model/Situation.java
+++ src/main/java/model/Situation.java
@@ -1,5 +1,17 @@
 package model;
 
+import controller.BinOpButton;
+import tools.State;
+
+import javax.swing.*;
+
 public class Situation {
-    //TODO
+    public State state = State.Input1; // init state
+    public JLabel display; // where numbers and results are shown
+    public BinOpButton binaryOperator;
+    public int leftOperand;
+
+    public Situation(JLabel display){
+        this.display = display;
+    }
 }
