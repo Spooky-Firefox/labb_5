976a1752395e3f4ed7e9254fff7c6c5edee2e03a
Alex <github@larus.se>
2023-02-24 07:04:37 +0100

    Kompletterade situation och la in cancel knapp som exempel

--- src/main/java/controller/CancelButton.java
+++ src/main/java/controller/CancelButton.java
@@ -9,6 +9,23 @@ public class CancelButton extends CalculatorButton{
 
     @Override
     public void transition() {
-        // TODO
+        switch (this.situation.state) {
+            case Input1:
+                this.situation.leftOperand = 0;
+                break;
+            case OpReady:
+                this.situation.leftOperand = 0;
+                // TODO: släck operator knapp!
+                break;
+            case Input2:
+                this.situation.leftOperand = 0;
+                this.situation.rightOperand = 0;
+                // TODO: släck operator knapp!
+                break;
+            case HasResult:
+                this.situation.leftOperand = 0;
+                // rightOperand bör bli 0:ad av = knappen ist
+                break;
+        }
     }
 }
--- src/main/java/model/Situation.java
+++ src/main/java/model/Situation.java
@@ -6,10 +6,11 @@ import tools.State;
 import javax.swing.*;
 
 public class Situation {
-    public State state = State.Input1; // init state
-    public JLabel display; // where numbers and results are shown
-    public BinOpButton binaryOperator;
-    public int leftOperand;
+    public State state = State.Input1; // Tillstånd för miniräknaren (se Håkans tillståndsdiagram)
+    public JLabel display;             // Visar första/andra talen när de slås in, sedan summan efter = knappen
+    public BinOpButton binaryOperator; // Vilken av de 4 räknesätten (operator)
+    public int leftOperand;            // Första talet
+    public int RightOperand;           // Andra talet (efter att operatorn ovan har valts)
 
     public Situation(JLabel display){
         this.display = display;
