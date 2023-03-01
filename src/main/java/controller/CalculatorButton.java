a1ada96e86f0ab4b2e4baeb59fe06b928069ff11
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-01 15:33:19 +0100

    added borders to CalculatorButton and removed magic color

--- src/main/java/controller/CalculatorButton.java
+++ src/main/java/controller/CalculatorButton.java
@@ -19,14 +19,19 @@ abstract class CalculatorButton extends JButton implements ActionListener {
         this.symbol = symbol;
         this.situation = situation;
 
-        // set color to gray
-        // TODO make Color.Gray a constant
-        this.setBackground(Color.gray);
+        this.setBackground(BUTTON_COLOR);
         this.setPreferredSize(new Dimension(BUTTON_SIZE, BUTTON_SIZE));
         this.setFont(FONT);
 
         // adding an action listener
         this.addActionListener(this);
+        this.setBorder(
+                BorderFactory.createCompoundBorder(
+                        BorderFactory.createLineBorder(KEYPAD_COLOR,MARGIN), // this is a workaround, since EmptyBorder
+                        // isn't transparent and lett the underlying object color be seen
+                        //BorderFactory.createEmptyBorder(MARGIN, MARGIN, MARGIN, MARGIN),
+                        BorderFactory.createLineBorder(BORDER_COLOR, BORDER_THICKNESS,ROUNDED_BORDER)
+                ));
     }
 
     public void actionPerformed(ActionEvent e) {
