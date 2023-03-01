60b9779eb46a2c8f2b9917faaed1401c8755594c
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-01 14:13:24 +0100

    added global constants

---
+++ src/main/java/tools/GlobalConstants.java
@@ -0,0 +1,8 @@
+package tools;
+
+import java.awt.*;
+
+abstract public class GlobalConstants {
+    public static final int BUTTON_SIZE = 100;
+    public static final Font FONT = new Font("Times New Roman", Font.BOLD, 85);
+}
