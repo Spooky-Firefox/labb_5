4a1f1c36b6cffa89dce8404620ebc4cf7a84eac2
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-01 14:57:57 +0100

    add two new global constants, margin and border thickness

--- src/main/java/tools/GlobalConstants.java
+++ src/main/java/tools/GlobalConstants.java
@@ -5,4 +5,6 @@ import java.awt.*;
 abstract public class GlobalConstants {
     public static final int BUTTON_SIZE = 100;
     public static final Font FONT = new Font("Times New Roman", Font.BOLD, 85);
+    public static final int MARGIN = 5;
+    public static final int BORDER_THICKNESS = 5;
 }
