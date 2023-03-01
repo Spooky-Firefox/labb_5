c872f7645ab95b0ea2c12ed63e67c4a0d641ec4c
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-01 15:29:37 +0100

    added more global constants

--- src/main/java/tools/GlobalConstants.java
+++ src/main/java/tools/GlobalConstants.java
@@ -7,4 +7,8 @@ abstract public class GlobalConstants {
     public static final Font FONT = new Font("Times New Roman", Font.BOLD, 85);
     public static final int MARGIN = 5;
     public static final int BORDER_THICKNESS = 5;
+    public static final Boolean ROUNDED_BORDER = false;
+    public static final Color KEYPAD_COLOR = new Color(128,128,128);
+    public static final Color BORDER_COLOR = Color.BLACK;
+    public static final Color BUTTON_COLOR = Color.WHITE;
 }
