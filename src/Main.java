d04b115a27e3fd339f92142c1a5b8cab3ccf78f7
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-06 16:13:25 +0100

    main now creates a gui

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
+import view.GUI;
+
 public class Main {
     public static void main(String[] args) {
-        //TODO create a instance of situation and GUI
-    	
-    	//Test David!
-    	System.out.println("test");
+		new GUI();
     }
 }
