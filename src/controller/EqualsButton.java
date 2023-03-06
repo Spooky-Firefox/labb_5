4c64e7d033562f0f85571bcbc33f27b770431fb3
yaboiyomokoishere <kalhed-2@student.ltu.se>
2023-03-06 17:48:41 +0100

    Update EqualsButton.java

--- src/controller/EqualsButton.java
+++ src/controller/EqualsButton.java
@@ -1,6 +1,9 @@
 package controller;
 
+import static tools.GlobalConstants.KEYPAD_COLOR;
+//import tools.State;
 import model.Situation;
+import tools.State;
 
 public class EqualsButton extends CalculatorButton{
     public EqualsButton(Situation situation) {
@@ -9,6 +12,15 @@ public class EqualsButton extends CalculatorButton{
 
     @Override
     public void transition() {
-        //TODO
+    	switch (this.situation.state) {
+        case Input1, OpReady, HasResult:
+            this.situation.display.setText(this.situation.display.getText()); //kanske inte behöver denna ens
+            break;
+        case Input2:
+            this.situation.display.setText("0");
+            this.situation.binaryOperator.setColor(KEYPAD_COLOR);
+            this.situation.state = State.HasResult;
+            break;
     }
+}
 }
