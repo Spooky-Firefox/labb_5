661332456c2639c8b9ee8a63f0e2bde9ee6959cc
Alex <github@larus.se>
2023-03-07 14:14:12 +0100

    Fix zero button

--- src/controller/DigitButton.java
+++ src/controller/DigitButton.java
@@ -5,32 +5,34 @@ import tools.State;
 import model.Situation;
 
 public class DigitButton extends CalculatorButton{
-    public DigitButton(String symbol, Situation situation) {
-        super(symbol, situation);
-    }
+	public DigitButton(String symbol, Situation situation) {
+		super(symbol, situation);
+	}
 
-    @Override
-    public void transition() {
-    	switch (this.situation.state) {
-    		case Input1, Input2:
-    			if (this.situation.display.getText()=="0") {
-    				System.out.println(super.symbol);
-    				this.situation.display.setText(super.symbol);
-    				break;
-    			}
-    			else {
-    			this.situation.display.setText(this.situation.display.getText()+ super.symbol);
-    			break;
-    			}
-    		case OpReady:
-                this.situation.display.setText(super.symbol);
-                this.situation.state = State.Input2;
-                break;
-    		case HasResult:
-    			this.situation.display.setText(super.symbol);
-    			this.situation.state = State.Input1;
-                break;
-    	}
+	@Override
+	public void transition() {
+		switch (this.situation.state) {
+			case Input1, Input2:
+				String text = this.situation.display.getText();
+					if (text.equals("0")) {
+						if (this.symbol.equals("0")) {
+							break;
+						}
+						this.situation.display.setText(this.symbol);
+						break;
+					}
+				this.situation.display.setText(text + this.symbol);
+				break;
 
-    }
+			case OpReady:
+				this.situation.display.setText(this.symbol);
+				this.situation.state = State.Input2;
+				break;
+
+			case HasResult:
+				this.situation.display.setText(this.symbol);
+				this.situation.state = State.Input1;
+				break;
+		}
+	}
 }
