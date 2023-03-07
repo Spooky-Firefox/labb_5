4589fa8a2c1d652090ff749453151fe6076ec1ed
Alex <github@larus.se>
2023-03-07 13:50:16 +0100

    removes comment with swedish letter and messy file encoding

--- src/controller/EqualsButton.java
+++ src/controller/EqualsButton.java
@@ -6,24 +6,24 @@ import model.Situation;
 import tools.State;
 
 public class EqualsButton extends CalculatorButton{
-    public EqualsButton(Situation situation) {
-        super("=", situation);
-    }
+	public EqualsButton(Situation situation) {
+		super("=", situation);
+	}
 
-    @Override
-    public void transition() {
-    	switch (this.situation.state) {
-        case Input1, OpReady, HasResult:
-        	this.situation.binaryOperator.setColor(KEYPAD_COLOR);
-            this.situation.display.setText(this.situation.display.getText()); //kanske inte behöver denna ens
-            break;
-        case Input2:
-        	Integer result = this.situation.binaryOperator.operator.applyAsInt(
-        			this.situation.leftOperand, Integer.parseInt(this.situation.display.getText()));
-            this.situation.display.setText(result.toString());
-            this.situation.binaryOperator.setColor(KEYPAD_COLOR);
-            this.situation.state = State.HasResult;
-            break;
-    }
-}
+	@Override
+	public void transition() {
+		switch (this.situation.state) {
+			case Input1, OpReady, HasResult:
+				this.situation.binaryOperator.setColor(KEYPAD_COLOR);
+				this.situation.display.setText(this.situation.display.getText());
+				break;
+			case Input2:
+				Integer result = this.situation.binaryOperator.operator.applyAsInt(
+						this.situation.leftOperand, Integer.parseInt(this.situation.display.getText()));
+				this.situation.display.setText(result.toString());
+				this.situation.binaryOperator.setColor(KEYPAD_COLOR);
+				this.situation.state = State.HasResult;
+				break;
+		}
+	}
 }
