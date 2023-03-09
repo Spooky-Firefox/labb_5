8876aa1bdf433f09597c08be841613f5ede625aa
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-09 13:28:29 +0100

    more javadoc for Situation

--- src/model/Situation.java
+++ src/model/Situation.java
@@ -8,9 +8,21 @@ import controller.BinOpButton;
  * Contains variabels and the state if the calculator such ass left operand, selected operator and display
  */
 public class Situation {
+    /**
+     * A variable representing the current state of the calculator using a Enum
+     */
     public State state = State.Input1; // Tillstånd för miniräknaren (se Håkans tillståndsdiagram)
+    /**
+     * The display (JLabel) associated with the calculator
+     */
     public final JLabel display;             // Visar första/andra talen när de slås in, sedan summan efter = knappen
+    /**
+     * The currently selected BinOPButton
+     */
     public BinOpButton binaryOperator; // Vilken av de 4 räknesätten (operator)
+    /**
+     * The left operand used for calculating the result
+     */
     public int leftOperand;            // Första talet
 
     /**
