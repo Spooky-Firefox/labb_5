ca0a3d4996fe67762cac5112341c9b3753b37e9e
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-09 13:06:50 +0100

    added javadoc to Situation

--- src/model/Situation.java
+++ src/model/Situation.java
@@ -4,12 +4,19 @@ import javax.swing.*;
 
 import controller.BinOpButton;
 
+/**
+ * Contains variabels and the state if the calculator such ass left operand, selected operator and display
+ */
 public class Situation {
     public State state = State.Input1; // Tillstånd för miniräknaren (se Håkans tillståndsdiagram)
     public final JLabel display;             // Visar första/andra talen när de slås in, sedan summan efter = knappen
     public BinOpButton binaryOperator; // Vilken av de 4 räknesätten (operator)
     public int leftOperand;            // Första talet
 
+    /**
+     * Returns the display of the calculator
+     * @param display the display
+     */
     public Situation(JLabel display){
         this.display = display;
     }
