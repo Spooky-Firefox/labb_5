0cb14090426514a3531705b5a07698b7f8c963ef
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-09 13:59:50 +0100

    added javadoc to BinOpButton

--- src/controller/BinOpButton.java
+++ src/controller/BinOpButton.java
@@ -7,14 +7,32 @@ import model.Situation;
 import static model.GlobalConstants.ACTIVE_COLOR;
 import static model.GlobalConstants.KEYPAD_COLOR;
 
+/**
+ * A button which purpose is the give state a method to apply operations on data, and as well as change state
+ */
 public class BinOpButton extends CalculatorButton{
-	public final IntBinaryOperator operator;
+	/**
+	 * A variable containing a IntBinaryOperator used for appalling the buttons operation
+	 */
+	public final IntBinaryOperator operator; // same thing as a private with a public getter method
 
+	/**
+	 * Creates BinOpButton with the given Text, situation and the operator
+	 * @param symbol the text to show to the user
+	 * @param situation the state of the application
+	 * @param operator the IntBinaryOperator describing the operation
+	 */
 	public BinOpButton(String symbol, Situation situation, IntBinaryOperator operator) {
 		super(symbol, situation);
 		this.operator = operator;
 	}
 
+	/**
+	 * Changes the state of Situation if Input1 or HasResult
+	 * ass well save the value in display to situation left operand.
+	 * including setting situation binaryOperator to the clicked button
+	 * if state is Input2 or OpReady change operator active operator
+	 */
 	@Override
 	public void transition() {
 		switch (this.situation.state) {
