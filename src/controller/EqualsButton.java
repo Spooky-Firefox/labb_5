c11209453d6f520f551e9b1eeb15d0ced307fc06
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-09 14:28:16 +0100

    added javadoc to EqualsButton

--- src/controller/EqualsButton.java
+++ src/controller/EqualsButton.java
@@ -4,11 +4,24 @@ import model.State;
 import model.Situation;
 import static model.GlobalConstants.KEYPAD_COLOR;
 
+/**
+ * A class that set the display to the calculated value
+ */
 public class EqualsButton extends CalculatorButton{
+
+	/**
+	 * Creates a EqualsButton
+	 * @param situation the state of the application
+	 */
 	public EqualsButton(Situation situation) {
 		super("=", situation);
 	}
 
+	/**
+	 * If the state is Input2 calculate the result of the operations with the value in leftOperand and display with
+	 * the selected BinOpButtons operation, sets the display and changes state to HasResult
+	 * if the state is not Input2 do nothing
+	 */
 	@Override
 	public void transition() {
 		// Do nothing for any state except Input2
